88b6cd9936125c8f54d910a2485b22ffee423a66
Krisli Dimo <krislidimo@gmail.com>
2019-05-20 17:52:26 -0500

    added actions

--- nifty-market-app/src/actions/index.js
+++ nifty-market-app/src/actions/index.js
@@ -7,3 +7,25 @@ export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const FETCH_DATA_START = 'FETCH_DATA_START';
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
 export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
+
+export const login = creds => dispatch => {
+  dispatch({ type: LOGIN_START });
+  return axios.post('', creds).then(res => {
+    localStorage.setItem('token', res.data.payload);
+    dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload });
+  }).catch(err => {
+  	dispatch({ type: LOGIN_FAILURE, error: err.response});
+  })
+};
+
+export const getData = () => dispatch => {
+  dispatch({ type: FETCH_DATA_START });
+  axios
+    .get('', )
+    .then(res => {
+      dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+};
