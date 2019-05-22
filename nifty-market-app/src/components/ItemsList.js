de0572b29278b42c59650da15a2191edd30df56b
Krisli Dimo <krislidimo@gmail.com>
2019-05-22 17:06:01 -0500

    added filters for shop items, wired display items to sever

--- nifty-market-app/src/components/ItemsList.js
+++ nifty-market-app/src/components/ItemsList.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { 
+	Card, 
+	CardImg, 
+	CardText, 
+	CardBody,
+	CardTitle, 
+	CardSubtitle, 
+	Button 
+} from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+import { getData } from '../actions/'
  
 class ItemsList extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			selectedCategory: '',
+		}
+	}
+
+	componentDidMount() {
+		this.props.getData();
+	}
+
 	render() {
 		return(
-			<div className="items-list">
-				<h2 className="title" >Title</h2>
+			<div className="items-list" >
+				<h2 className="title" >Market</h2>
+				<Form className="filters" inline>
+	        <FormGroup>
+	          <Label className='label' for="exampleSelect">{`Catagory:   `}</Label>
+	          <Input type="select" name="select" id="exampleSelect">
+	            <option>Cards</option>
+	            <option>Video Games</option>
+	          </Input>
+	        </FormGroup>
+	      </Form>
 				<div className="cards-container" >
 					{this.props.items.map( item => { 
-						return <Card key={item.id}>
-			        <CardImg top width="100%" src="item.img" alt="Card image cap" />
+						return <Card className="text-center" key={item.id} >
 			        <CardBody>
-			          <CardTitle>{item.name}</CardTitle>
-			          <CardText>{item.description}</CardText>
+			        	<CardTitle>{item.name}</CardTitle>
+			        </CardBody>
+		        	<div className='img-container'> 
+		        		<CardImg className="card-img" top width="100%" src={item.imgUrl} alt="Card image cap" />
+		          </div>
+			        <CardBody>
+			          <CardText className='price'>$9.99</CardText>
+			          <Button>Add to Wishlist</Button>
 			          <Button>Buy Now</Button>
 			        </CardBody>
 			      </Card>
@@ -29,4 +65,4 @@ const mapStateToProps = ({ items }) => ({
 	items
 });
 
-export default connect(mapStateToProps, {})(ItemsList);
+export default connect(mapStateToProps, { getData })(ItemsList);
